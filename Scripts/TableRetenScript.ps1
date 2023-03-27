8bf2a3d97090f33034a9ce65208cdf8f700b64b0
mathewOrtiz <119885367+mathewOrtiz@users.noreply.github.com>
2023-03-27 16:43:05 -0400

    This adds in functions so that we can use this script to pull additional info. This also provides a more fluid of way of querying data for the table names.

--- Scripts/TableRetenScript.ps1
+++ Scripts/TableRetenScript.ps1
@@ -1,6 +1,8 @@
 $RetentionDays = 90
 $RetentionTotal = 365
 
+## The below function is used in order to set the retention for all of our tables to the standard 1 year setting. 
+function RetentionSet{
 #Creates a array with all of the subscriptions. 
 $Subscriptions = @(az account list --query '[].name')
 
@@ -13,15 +15,25 @@ foreach ($Subscription in Subscriptions){
     $ResourceGroups = az group list --query '[].name'
     $WorkspaceNames = az monitor log-analytics workspace list --query '[].name'
 
+    if($WorkspaceNames -ne "" ){
     #Collects the necessary table names
-    $tables = @(az monitor log-analytics workspace table list --resource-group $ResourceGroups --workspace-name $WorkspaceNames )
+    $tables = @(az monitor log-analytics workspace table list --resource-group $ResourceGroups --workspace-name $WorkspaceNames --query '[].name' )
+    foreach ($name in $names){
+        Update-AzOperationalInsightsTable -ResourceGroupName $RgName -WorkspaceName $WorkName -TableName $name -RetentionInDays $RetenDays -TotalRetentionInDays $RetenTotal
+        }
+    }
+    #the following will run when the LAW is empty
+    else{
+        continue
+    }
 
     #the following will actually work through every table in the list to modify the retention that is set to meet our standards.
 
-    foreach($table in $tables){
-        az monitor log-analytics workspace table update --resource-group $ResourceGroups --workspace-name $WorkspaceNames
+    foreach ($name in $names){
+        Update-AzOperationalInsightsTable -ResourceGroupName $RgName -WorkspaceName $WorkName -TableName $name -RetentionInDays $RetenDays -TotalRetentionInDays $RetenTotal
+        }
+    
+    
+    
     }
-    
-
-    
 }
