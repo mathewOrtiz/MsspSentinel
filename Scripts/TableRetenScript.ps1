a1f753898134e7276c9890c5d28cef97d346ff6e
mathewOrtiz <119885367+mathewOrtiz@users.noreply.github.com>
2023-03-28 14:12:45 -0400

    Corrected syntax error with table update api

--- Scripts/TableRetenScript.ps1
+++ Scripts/TableRetenScript.ps1
@@ -2,7 +2,7 @@ $RetentionDays = 90
 $RetentionTotal = 365
 
 ## The below function is used in order to set the retention for all of our tables to the standard 1 year setting. 
-function RetentionSet{
+function RetentionSetAllCustomers{
 #Creates a array with all of the subscriptions. 
 $Subscriptions = @(az account list --query '[].name')
 
@@ -11,8 +11,8 @@ foreach ($Subscription in Subscriptions){
 
     #This line ensures that we are located in the first subscription out of our array that we created.
     az context set $Subscriptions
-    #This will prompt you to enter the resource group for the subscription.
-    $ResourceGroups = az group list --query '[].name'
+    #This will pull the Rgs and pull out only the ones that we created.
+    $ResourceGroups = az group list --query '[].name' | Select-String 'H\d{6}'
     #The following below should only pull log analytics workspaces that are in line with our naming convention.
     $WorkspaceNames = az monitor log-analytics workspace list --query '[].name' | Select-String -Pattern 'H\d{6}'
 
@@ -20,12 +20,9 @@ foreach ($Subscription in Subscriptions){
     #Collects the necessary table names
     $tables = @(az monitor log-analytics workspace table list --resource-group $ResourceGroups --workspace-name $WorkspaceNames --query '[].name' )
     foreach ($table in $tables){
-        az monitor log-analytics workspace table update --resource-group $ResourceGroups --workspace-name $WorkspaceNames -n $table --retenetion-days $RetentionDays --total-retention-time $RetentionTotal
+        az monitor log-analytics workspace table update --resource-group $ResourceGroups --workspace-name $WorkspaceNames -n $table --retention-time $RetentionDays --total-retention-time $RetentionTotal
         }
     }
     #the following will run when the LAW is empty
-    else{
-        continue
-    }
     }
 }
