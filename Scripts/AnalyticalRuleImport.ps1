7d5c0f4d8563b38d145eaab25dc82beb6245f9b6
mathewOrtiz <119885367+mathewOrtiz@users.noreply.github.com>
2023-05-12 18:23:35 -0400

    Improved menu function

--- Scripts/AnalyticalRuleImport.ps1
+++ Scripts/AnalyticalRuleImport.ps1
@@ -1,3 +1,5 @@
+#Leverage SAS for the pull down of URLs.
+
 $LinuxRules = "someurl"
 $WinRules = "someURl"
 
@@ -14,17 +16,31 @@ $Subscription = Read-Host "what is the customer account which you would like to 
 az account list --query '[].name' --output table
 
 az context set -s $Subscription
-
-Write-Host "You will need to know the log analytics workspace of the customer. 
-
-Unsure? Select one of our greatest "
-
 function ImportAll{
-$WorkSpaceName = az monitor log-analytics workspace list --query '[].name'
+$WorkSpaceName = az monitor log-analytics workspace list --query '[].name' | Select-String -Pattern "AzureSentinel"
 $ResourceGroups = az group list --query '[].name' --output table | Select-String 'AzureSentinel'
 
 foreach($Rule in $Rules){
     #The below needs to be modified with the template uri instead of template file initialization. This will ensure that we can use a array which has the values of all our template files. 
     az deployment group create --resource-group $ResourceGroups --template-file $Rule --parameter worksapce="$WorkSpaceName"
 }
+}
+function menu{
+$validResponse = @{
+    "Import All Rules Available" = {ImportAll}
+    "Primary (Win,Linux,Multi-Source,Network)" = {Primary}
+    "Specify Name" = {SpecifyName}
+    "All rule" = {AllRule}
+    "Audit Existing" = {Audit}
+}
+do{
+    $response = Read-Host "Please select one of the available utilities 
+1.) All
+2.) Primary (Win,Linux,Multi-Source,Network)
+3.) Specify name (Insert list of names for rules which can likely be done through the creation of a array for the names.)
+4.) All Rule types
+5.) List out Existing rules"
+}until ($validResponse.ContainsKey($response))
+
+& $validResponse[$response]
 }
